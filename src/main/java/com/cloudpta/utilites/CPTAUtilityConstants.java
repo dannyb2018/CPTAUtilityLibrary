99825025c1a070669da96f8c45228e34b60f215f
dannyb2018 <39492136+dannyb2018@users.noreply.github.com>
2020-01-10 23:30:43 +0900

    date based constants

---
+++ src/main/java/com/cloudpta/utilites/CPTAUtilityConstants.java
@@ -0,0 +1,37 @@
+/*
+
+Copyright 2017-2019 Advanced Products Limited, 
+dannyb@cloudpta.com
+github.com/dannyb2018
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+package com.cloudpta.utilites;
+
+/**
+ *
+ * @author Danny
+ */
+public interface CPTAUtilityConstants
+{
+    public static final String CPTA_DATE_FORMAT = "yyyy-MM-dd";
+    public static final String CPTA_DATE_TIME_FORMAT = "yyyy-MM-dd";
+
+    public static final String CPTA_OFFSET_PROPERTY_DAILY = "D";
+    public static final String CPTA_OFFSET_PROPERTY_WEEKLY = "W";
+    public static final String CPTA_OFFSET_PROPERTY_MONTHLY = "M";        
+    public static final String CPTA_OFFSET_PROPERTY_YEARLY = "Y";        
+
+    public static final String DATE_FIELD_NAME = "date";    
+}

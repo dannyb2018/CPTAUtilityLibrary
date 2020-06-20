5864f3de48f45b67772f9d7932da10c193d0d477
dannyb2018 <39492136+dannyb2018@users.noreply.github.com>
2020-06-21 00:14:36 +0900

    test for cpta exceptions

---
+++ src/test/java/com/cloudpta/utilites/exceptions/CPTAExceptionTest.java
@@ -0,0 +1,71 @@
+/*
+
+Copyright 2017-2019 Advanced Products Limited, 
+dannyb@cloudpta.com
+github.com/dannyb2018
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+package com.cloudpta.utilites.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.json.JsonObject;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author Danny
+ */
+public class CPTAExceptionTest
+{
+    /**
+     * Test of getErrors method, of class CPTAException.
+     */
+    @Test
+    public void testGetErrors()
+    {
+        try
+        {
+            String x = null;
+            x.getBytes();
+        }
+        catch(NullPointerException e)
+        {
+            CPTAException instance = new CPTAException(e);
+            JsonObject errors = instance.getErrors();
+            System.out.println(errors.toString());
+        }
+        
+        Exception e = new Exception("test");
+        CPTAException instance = new CPTAException(e);
+        JsonObject errors = instance.getErrors();
+        System.out.println(errors.toString());
+        
+        List<String> testArray = new ArrayList<>();
+        try
+        {
+        testArray.get(0);
+        }
+        catch(Exception e2)
+        {
+        instance = new CPTAException(e2);
+        errors = instance.getErrors();
+        System.out.println(errors.toString());
+        
+            
+        }
+    }
+    
+}

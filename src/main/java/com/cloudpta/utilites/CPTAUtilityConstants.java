11fa2893a97b0f3e008ada6f1a324b63bced7b77
dannyb2018 <39492136+dannyb2018@users.noreply.github.com>
2020-10-23 11:59:02 +0900

    added logging

--- src/main/java/com/cloudpta/utilites/CPTAUtilityConstants.java
+++ src/main/java/com/cloudpta/utilites/CPTAUtilityConstants.java
@@ -38,4 +38,28 @@ public interface CPTAUtilityConstants
     public static final String CPTA_OFFSET_PROPERTY_YEARLY = "Y";        
 
     public static final String DATE_FIELD_NAME = "date";    
+    
+    public final static String LOG_PATTERN_PROPERTY = "LOG_PATTERN";    
+    public final static String LOG_PATTERN_PROPERTY_DEFAULT = "%d{yyyy:MM:dd-HH:mm:ss:SSS} [%p|%c|%C{1}] %m%n";    
+    public final static String LOG_MAX_FILE_SIZE_PROPERTY = "MAX_FILE_SIZE";    
+    public final static String LOG_MAX_FILE_SIZE_PROPERTY_DEFAULT = "50MB";    
+    public final static String LOG_CONSOLE_THRESHOLD_PROPERTY = "CONSOLE_THRESHOLD";    
+    public final static String LOG_FILE_THRESHOLD_PROPERTY = "FILE_THRESHOLD";    
+    public final static String LOG_THRESHOLD_PROPERTY_OFF = "OFF";
+    public final static String LOG_FILE_THRESHOLD_PROPERTY_DEFAULT = "OFF";    
+    public final static String LOG_CONSOLE_THRESHOLD_PROPERTY_DEFAULT = "ALL";    
+    public final static String LOG_THRESHOLD_PROPERTY_ALL = "ALL";    
+    public final static String LOG_THRESHOLD_PROPERTY_TRACE = "TRACE";    
+    public final static String LOG_THRESHOLD_PROPERTY_WARN = "WARN";    
+    public final static String LOG_THRESHOLD_PROPERTY_DEBUG = "DEBUG";    
+    public final static String LOG_THRESHOLD_PROPERTY_ERROR = "ERROR";    
+    public final static String LOG_THRESHOLD_PROPERTY_INFO = "INFO";    
+    public final static String LOG_FILE_NAME_PATTERN_PROPERTY = "FILE_NAME_PATTERN";
+    public final static String LOG_FILE_NAME_PATTERN_PROPERTY_DEFAULT = "/opt/api-server/logs/qpapiserver.%d.log.gz";
+    public final static String LOG_FILE_MAX_HISTORY_PROPERTY = "MAX_HISTORY";
+    public final static String LOG_FILE_MAX_HISTORY_PROPERTY_DEFAULT = "30";
+    public final static String LOG_FILE_TOTAL_MAX_SIZE_PROPERTY = "TOTAL_MAX_LOG_SIZE";
+    public final static String LOG_FILE_TOTAL_MAX_SIZE_PROPERTY_DEFAULT = "3GB";
+    
+    
 }

ab576b7461089242f74e5367a6a6f10ec8ebe285
dannyb2018 <39492136+dannyb2018@users.noreply.github.com>
2022-03-04 03:21:10 +0900

    added a library version

--- src/main/java/com/cloudpta/utilites/CPTAUtilityConstants.java
+++ src/main/java/com/cloudpta/utilites/CPTAUtilityConstants.java
@@ -25,6 +25,8 @@ package com.cloudpta.utilites;
  */
 public interface CPTAUtilityConstants
 {
+    public static final String VERSION = "1.3.1";        
+
     public static final String EXCEPTION_MESSAGE_FIELD = "message";
     public static final String ERROR_LINE_MESSAGE_FIELD = "error_line";
     public static final String STACK_TRACE_FIELD = "stack_trace";

7e8d95679f5a84822ee1af8a22e7ca7164554e03
dannyb2018 <39492136+dannyb2018@users.noreply.github.com>
2022-05-10 12:23:30 +0900

    bumped version

--- src/main/java/com/cloudpta/utilites/CPTAUtilityConstants.java
+++ src/main/java/com/cloudpta/utilites/CPTAUtilityConstants.java
@@ -25,7 +25,7 @@ package com.cloudpta.utilites;
  */
 public interface CPTAUtilityConstants
 {
-    public static final String VERSION = "1.5.5";       
+    public static final String VERSION = "1.5.6";       
     
     public static final String FRONTEND_LOCATION_PROPERTY_NAME = "FRONTEND_LOCATION";
     public static final String SERVER_PORT_PROPERTY_NAME = "SERVER_PORT";

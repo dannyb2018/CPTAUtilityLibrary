e88b97b727bec8a280ab34ab92fbef908bcabc3d
dannyb2018 <39492136+dannyb2018@users.noreply.github.com>
2022-07-04 22:37:09 +0900

    can create a type programmatically

---
+++ src/main/java/com/cloudpta/graphql/common/CPTAGraphQLDynamicObject.java
@@ -0,0 +1,59 @@
+/*
+
+Copyright 2017-2019 Advanced Products Limited, 
+Copyright 2021-2022 Liquid Markets Limited, 
+github.com/dannyb2018
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+package com.cloudpta.graphql.common;
+
+import java.util.List;
+import graphql.language.Description;
+import graphql.language.FieldDefinition;
+import graphql.language.ObjectTypeDefinition;
+import graphql.language.Type;
+import graphql.schema.idl.TypeDefinitionRegistry;
+
+public abstract class CPTAGraphQLDynamicObject 
+{
+    public void addToTypeRegistry(TypeDefinitionRegistry apiTypeDefinitionRegistry)
+    {
+        // create the object type definition for this object
+        // Get the name of the object
+        String objectName = getClass().getSimpleName();
+        ObjectTypeDefinition.Builder dynamicObjectTypeDefinitionBuilder = ObjectTypeDefinition.newObjectTypeDefinition().name(objectName);
+        // add description
+        Description description = getDescription();
+        dynamicObjectTypeDefinitionBuilder.description(description);
+        // If there is a non-null interface
+        Type possibleInterface = getInterface();
+        if(null != possibleInterface)
+        {
+            dynamicObjectTypeDefinitionBuilder.implementz(possibleInterface);
+        }
+
+        // Add fields
+        List<FieldDefinition> fieldDefinitions = getFieldDefinitions();
+        dynamicObjectTypeDefinitionBuilder.fieldDefinitions(fieldDefinitions);
+
+        // add to api type registry
+        ObjectTypeDefinition typeDefinitionOfThisObject = dynamicObjectTypeDefinitionBuilder.build();
+        apiTypeDefinitionRegistry.add(typeDefinitionOfThisObject); 
+    }
+    
+    protected abstract List<FieldDefinition> getFieldDefinitions();
+    protected abstract Description getDescription();
+    protected abstract Type getInterface();
+}
